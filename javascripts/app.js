d2052a543acada9fbd4882a669668a52409b8f65
Martin Cross <mncross@gmail.com>
2015-10-29 21:07:30 -0500

    How can I modify properties of the inherited prototype?

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,17 +1,87 @@
-requirejs.config({
-  baseUrl: './javascripts',
-  paths: {
-    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
-    'lodash': '../lib/bower_components/lodash/lodash.min',
-    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
-    'q': '../lib/bower_components/q/q'
-  }
+$(document).ready(function() {
+
+  /*
+    // Test code to generate a human player and an orc player
+  var warrior = new Human();
+  warrior.setWeapon(new WarAxe());
+  warrior.generateClass();  // This will be used for "Surprise me" option
+  console.log(warrior.toString());
+  console.log("warrior", warrior);
+
+  var orc = new Orc();
+  orc.generateClass();
+  orc.setWeapon(new BroadSword());
+  console.log(orc.toString());
+  console.log("orc", orc);
+
+    // Test code to generate a spell
+  var spell = new Sphere();
+  console.log("spell: ", spell.toString());
+  console.log("spell", spell);
+
+    END OF TEST CODE
+  */
+
+  /*
+    Show the initial view that accepts player name
+   */
+  $("#player-setup").show();
+  var userPlayer;
+  var userPlayerName;
+  var userPlayerClass;
+
+  $('.class__link').click(function(e) {
+    userPlayerClass = $(this).children('.btn__text').text();
+    if (userPlayerClass === "surprise me") {
+      userPlayerClass = userPlayer.generateClass();
+      console.log("userPlayerClass", userPlayerClass);
+    } else {
+      console.log("userPlayerClass", userPlayerClass);
+    }
+
+    //this line should udpate the inherited class property from null to the user's selected class, but instead it just adds a duplicate class property to the Human prototype.
+    userPlayer.class = userPlayerClass;
+    console.log("userPlayer.class", userPlayer.class);
+    console.log("userPlayer", userPlayer);
+  });
+
+  /*
+    When any button with card__link class is clicked,
+    move on to the next view.
+   */
+  $(".card__link").click(function(e) {
+    var nextCard = $(this).attr("next");
+    var moveAlong = false;
+
+    switch (nextCard) {
+      case "card--class":
+        moveAlong = ($("#player-name").val() !== "");
+        userPlayerName = $("#player-name").val();
+        console.log("userPlayerName", userPlayerName);
+        userPlayer = new Human();
+
+        //this line should udpate the inherited playerName property to the user's provided name, but instead it just adds a duplicate playerName property to the Human prototype.
+        userPlayer.setName(userPlayerName);
+        console.log("userPlayer", userPlayer);
+        break;
+      case "card--weapon":
+        moveAlong = ($("#player-name").val() !== "");
+        break;
+    }
+
+    if (moveAlong) {
+      $(".card").hide();
+      $("." + nextCard).show();
+    }
+  });
+
+  /*
+    When the back button clicked, move back a view
+   */
+  $(".card__back").click(function(e) {
+    var previousCard = $(this).attr("previous");
+    $(".card").hide();
+    $("." + previousCard).show();
+  });
+
 });
-
-requirejs(
-  ["jquery", "hbs", "q", "classes", "enemies","player","spells"],
-  function($, Handlebars, q, classes, enemies, player, spells) {
-
-console.log('wired up');
-
-}); //end require
--- javascripts/enemies.js
+++ javascripts/enemies.js
@@ -1,12 +1,20 @@
-define(function(require) {
-  var $ = require("jquery"),
-      _ = require("lodash"),
-  		q = require("q");
+var Orc = function() {
+  this.health = this.health + 20;
+  this.species = "Orc";
+  this.allowedClasses = ["Warrior", "Berserker", "Shaman"];
 
+  this.generateClass = function() {
+    // Get a random index from the allowed classes array
+    var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
 
-  return {
+    // Get the string at the index
+    var randomClass = this.allowedClasses[random];
 
+    // Composes the corresponding player class into the player object
+    this.class = new window[randomClass]();
+    return this.class;
+  };
+};
 
-	};//end return
+Orc.prototype = new Monster();
 
-}); //end define
